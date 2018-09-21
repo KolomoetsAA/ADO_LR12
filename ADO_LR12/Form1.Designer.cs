5f65659f4a71b276ecf8d6f9766e60f194da04bf
KolomoetsAA <favcr5@gmail.com>
2018-09-21 21:02:28 +0300

    commit 1

---
+++ ADO_LR12/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ADO_LR12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton_Name = new System.Windows.Forms.RadioButton();
+            this.radioButton_Surname = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(297, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // radioButton_Name
+            // 
+            this.radioButton_Name.AutoSize = true;
+            this.radioButton_Name.Checked = true;
+            this.radioButton_Name.Location = new System.Drawing.Point(303, 0);
+            this.radioButton_Name.Name = "radioButton_Name";
+            this.radioButton_Name.Size = new System.Drawing.Size(53, 17);
+            this.radioButton_Name.TabIndex = 1;
+            this.radioButton_Name.TabStop = true;
+            this.radioButton_Name.Text = "Name";
+            this.radioButton_Name.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_Surname
+            // 
+            this.radioButton_Surname.AutoSize = true;
+            this.radioButton_Surname.Location = new System.Drawing.Point(394, 0);
+            this.radioButton_Surname.Name = "radioButton_Surname";
+            this.radioButton_Surname.Size = new System.Drawing.Size(67, 17);
+            this.radioButton_Surname.TabIndex = 2;
+            this.radioButton_Surname.TabStop = true;
+            this.radioButton_Surname.Text = "Surname";
+            this.radioButton_Surname.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(868, 480);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Location = new System.Drawing.Point(485, 0);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(324, 20);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(815, 0);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(53, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 506);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.radioButton_Surname);
+            this.Controls.Add(this.radioButton_Name);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton_Name;
+        private System.Windows.Forms.RadioButton radioButton_Surname;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ADO_LR12/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO_LR12
+{
+    public partial class Form1 : Form
+    {
+        PublishingHouseEntities db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new PublishingHouseEntities();
+            var books = from b in db.Books
+                        join a in db.Authors on b.ID_AUTHOR equals a.ID_AUTHOR
+                        select new { Titel = b.NameBook, Name = a.FirstName, Surname = a.LastName, Price = b.Price};
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = books.ToList();
+            trackBar1.Value = (int)db.Books.Max(b => b.Price);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            if (radioButton_Name.Checked)
+            {
+                var books = from b in db.Books
+                            join a in db.Authors on b.ID_AUTHOR equals a.ID_AUTHOR
+                            where a.FirstName.Contains(textBox1.Text)
+                            select new { Titel = b.NameBook, Name = a.FirstName, Surname = a.LastName, Price = b.Price };
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = books.ToList();
+
+            }
+            else
+            {
+                var books = from b in db.Books
+                            join a in db.Authors on b.ID_AUTHOR equals a.ID_AUTHOR
+                            where a.LastName.Contains(textBox1.Text)
+                            select new { Titel = b.NameBook, Name = a.FirstName, Surname = a.LastName, Price = b.Price };
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = books.ToList();
+            }
+            
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = trackBar1.Value.ToString();
+            if (radioButton_Name.Checked)
+            {
+                var books = from b in db.Books
+                            join a in db.Authors on b.ID_AUTHOR equals a.ID_AUTHOR
+                            where a.FirstName.Contains(textBox1.Text) && b.Price <= trackBar1.Value
+                            select new { Titel = b.NameBook, Name = a.FirstName, Surname = a.LastName, Price = b.Price };
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = books.ToList();
+
+            }
+            else
+            {
+                //var books = from b in db.Books
+                //            join a in db.Authors on b.ID_AUTHOR equals a.ID_AUTHOR
+                //            where a.LastName.Contains(textBox1.Text) && b.Price <= trackBar1.Value
+                //            select new { Titel = b.NameBook, Name = a.FirstName, Surname = a.LastName, Price = b.Price };
+
+                var books = db.Books.Join(
+                    db.Authors, //что присоеденяем
+                    b => b.ID_AUTHOR, //по какому полю связь по первой таблице
+                    a => a.ID_AUTHOR, //по какому полю связь по второй таблице
+                    (b, a) => new { Titel = b.NameBook, Name = a.FirstName, Surname = a.LastName, Price = b.Price }) //что выбираем (какие поля будут в результате)
+                    .Where(j => j.Surname.Contains(textBox1.Text) && j.Price <= trackBar1.Value); //условие
+
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = books.ToList();
+            }
+        }
+    }
+}
